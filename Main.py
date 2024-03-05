59280fd8fc73224e1bfcb5139cf8b52b664d5049
dewirees20 <66516675+dewirees20@users.noreply.github.com>
2024-03-05 10:23:14 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
-from flask import Flask
-import pandas as pd
+from flask import Flask, render_template
+# import pandas as pd
 
 # print("Hello")
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello"
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
