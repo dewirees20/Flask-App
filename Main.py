90271301f51d2250c4803c5ada18455c5cd7d2f2
Dewi Rees <dewi.rees@protonmail.com>
2024-03-07 15:49:11 +0000

    DWR: Added CSS and updated HTML

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 
 # print("Hello")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index_copy.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
